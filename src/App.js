83fe4858e3059d120ef8fa7da1576fe63b74e406
Kevin4153 <kevin.ayala.school415@gmail.com>
2022-10-15 21:37:25 -0500

    compares correctly-kinda

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
   const [progress, setProgress] = useState(0);
   const [result, setResult] = useState("");
 
+
+  const not_good = ["cocoa"]; // list of bad ing
+  const [value, setValue] = useState("Good"); // value to return good or bad
+  let badIngre = ""; // ingr that was bad
+
   const onFileChange = (e) => {
     //console.log(e.target.files);
     setFile(e.target.files[0]);
@@ -22,8 +27,38 @@ function App() {
       },
     }).then(({ data: { text } }) => {
       setResult(text);
+      checkIngredients();
     });
+    
   };
+
+  const checkIngredients = () => {
+    if (result !== "") {
+      // split ing based on commas
+      const arrInput = result.split(',')
+      console.log(arrInput);
+
+      // loop through list of user ingred
+      for (let i = 0; i < arrInput.length; i++) {
+
+        let currWord = arrInput[i].toLowerCase();
+
+        currWord = currWord.trim();
+
+        //console.log(currWord === not_good[0]);
+        for (let j = 0; j < not_good.length; j++) {
+
+          if (currWord === not_good[j]) {
+            
+            setValue("Bad");
+            badIngre = currWord;
+            
+            break;
+          }
+        }
+      }
+    }
+  }
 
   return (
     <div className="App">
@@ -40,6 +75,14 @@ function App() {
         Result: {result}
       </div>)} 
 
+      <div> 
+        { (<div style={{marginTop: 20, fontSize: 24, color: "teal"}}>
+        
+        Value: {value}
+
+      </div>)}
+
+      </div>
     </div>
   );
 }
